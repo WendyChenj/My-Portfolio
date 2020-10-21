02cc62c1941ed3d98f283e5d512a2354e56ddc3b
Wenjie Chen <wenjiechen@WenjiedeMacBook-Air.local>
2020-10-21 16:36:26 -0400

    add portfolio images into media branch

---
+++ build/precache-manifest.a581d72ccdaa7e3d3b0788e54e9ff99c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f62bd483acbbbf6bcd6b7dd9a244fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb22f98ed222ab3d14b7",
+    "url": "/static/css/main.0ce10f5e.chunk.css"
+  },
+  {
+    "revision": "eed095fcfd1e2767c709",
+    "url": "/static/js/2.29d2d74b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.29d2d74b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb22f98ed222ab3d14b7",
+    "url": "/static/js/main.5d75a155.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "204a9a0a9a62d7f9c8e191bfba8664e7",
+    "url": "/static/media/1-home-page.204a9a0a.png"
+  },
+  {
+    "revision": "8741a60a28ffef035a204ea481c532b9",
+    "url": "/static/media/2-account-page.8741a60a.png"
+  },
+  {
+    "revision": "10d09b22c7e345423a02bd864c023bd8",
+    "url": "/static/media/3-ingredients-page.10d09b22.png"
+  },
+  {
+    "revision": "171e40db92ba961357a4e6555e3dc0e9",
+    "url": "/static/media/4-contact-form-page.171e40db.png"
+  },
+  {
+    "revision": "31c65e4986eea17f891a3ea2906b95fa",
+    "url": "/static/media/5-orders-page.31c65e49.png"
+  },
+  {
+    "revision": "55d3edf4d9cedaf0a69e500fa8896b37",
+    "url": "/static/media/6-responsive-home-page.55d3edf4.png"
+  },
+  {
+    "revision": "9cbd50734bbacbbdeee53b4db8cc2794",
+    "url": "/static/media/7-responsive-ingres-page.9cbd5073.png"
+  },
+  {
+    "revision": "248f8e777a3df3e2ee80862cfb28d26b",
+    "url": "/static/media/8-responsive-account-page.248f8e77.png"
+  },
+  {
+    "revision": "a3fcd61b66196ec01cc99f2209939174",
+    "url": "/static/media/9-responsive-orders-page.a3fcd61b.png"
+  },
+  {
+    "revision": "bd8c9d707998e4e1c928be91dcd2bfdb",
+    "url": "/static/media/self-small.bd8c9d70.png"
+  },
+  {
+    "revision": "4521adc6f62578a2e9b4686505d9a210",
+    "url": "/static/media/self.4521adc6.JPG"
+  }
+]);
