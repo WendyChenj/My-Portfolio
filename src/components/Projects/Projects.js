1f8a35f0d65231121bfe7312edbaefbce8efe49b
Wenjie Chen <wenjiechen@WenjiedeMacBook-Air.local>
2020-11-14 12:58:38 -0500

    update projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,37 +1,13 @@
 import React from 'react';
 import ContentGrid from '../Layout/ContentGrid'; 
-import { Divider, Typography, Chip, GridList, GridListTile, Link, Hidden } from '@material-ui/core';
+import { Divider, Typography, Chip, Link, Hidden } from '@material-ui/core';
 import FastfoodIcon from '@material-ui/icons/Fastfood';
 import DirectionsBusIcon from '@material-ui/icons/DirectionsBus';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import LinkIcon from '@material-ui/icons/Link';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import './Projects.css';
-
-// use require.context() in webpack: https://webpack.js.org/guides/dependency-management/#require-context
-function importAll(r) {
-    let images = {};
-    // eslint-disable-next-line
-    r.keys().map( item => { 
-        images[item.replace('./', '')] = r(item); 
-    });
-    return images;
-}
-
-const images = importAll(require.context( '../../assets/images/burgerProject', false, /\.(png|jpe?g|svg)$/));
-
-const BurgerProjectImgs = () => {
-    return (
-        <GridList cellHeight={window.innerWidth > 700 ? 400: 300 } style={{ flexWrap: 'nowrap', transform:'translateZ(0)'}}>
-            {Object.keys(images).map( img => {
-                return (
-                    <GridListTile cols={1} rows={1} key={img} >
-                        <img src={images[img]} alt={img} className='project-image'/>
-                    </GridListTile>
-                )}
-            )}
-        </GridList>
-    );
-}
+import BurgerProjectImgs from './BurgerProjectImgs/BurgerProjectImgs';
  
 const Projects = () => {
 
@@ -50,7 +26,7 @@ const Projects = () => {
                         <Typography color='primary' variant='h6' style={{fontWeight: '800', paddingBottom: '8px'}}>2020.06-2020.09</Typography> 
                         <div className='project-item-link'>
                             <Typography variant='h5' style={{fontWeight: '800', paddingBottom: '8px'}}>
-                                My Burger Order Application
+                                Burger Order E-commerce Application
                                 <Link href='https://react-my-burger-1f18b.web.app/' target='_blank' rel='noopener'>
                                     <LinkIcon style={{paddingBottom: '8px', paddingLeft: '8px'}} />
                                 </Link>
@@ -73,6 +49,48 @@ const Projects = () => {
 
                         <div>
                             <BurgerProjectImgs  />
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <Hidden xsDown>
+                    <Divider variant='inset'/>
+                </Hidden>
+
+                <Hidden smUp>
+                    <Divider />
+                </Hidden>
+
+                <div className='project-item'>
+                    <div className='project-item-icon'>
+                        <SupervisorAccountIcon color='primary' fontSize='large'  />
+                    </div>
+                    <div className='project-images-title-container'>
+                        <Typography color='primary' variant='h6' style={{fontWeight: '800', paddingBottom: '8px'}}>2020.01-2020.02</Typography> 
+                        <div className='project-item-link'>
+                            <Typography variant='h5' style={{fontWeight: '800', paddingBottom: '8px'}}>
+                                Admin Dashboard Application
+                                <Link href='https://nostalgic-brattain-51149d.netlify.app/' target='_blank' rel='noopener'>
+                                    <LinkIcon style={{paddingBottom: '8px', paddingLeft: '8px'}} />
+                                </Link>
+                                <Link href='https://github.com/WendyChenj/Admin-Dashboard' target='_blank' rel='noopener'>
+                                    <GitHubIcon style={{paddingBottom: '8px', paddingLeft: '8px'}} />
+                                </Link>
+                            </Typography>  
+                        </div> 
+                                              
+                        <Typography color='secondary' style={{paddingBottom: '8px'}}>
+                            The dashboard allows users to check the sales performance of products and track the progress of team members. 
+                            Users also could add new tasks and delete tasks that have been done. 
+                            Moreover, there is a monthly timeline to remind users big event and the date when big event happens. 
+                            Users could edit the date and event according to their own schedule.
+                        </Typography>
+                        <div className='project-item-tech-stack'>
+                            <Typography style={{fontWeight: '800', paddingRight: '16px'}}>Tech Stack: </Typography>
+                            {[ 'React', 'Redux', 'Material-UI', 'react-router', 'D3.js' ].map( tech => {
+                                return <Chip label={tech} color='primary' key={tech} className='project-item-tech-stack-item' />
+                            })}
                         </div>
                         
                     </div>
