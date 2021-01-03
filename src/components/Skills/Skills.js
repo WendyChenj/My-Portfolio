0d79a86c8ace60a134e5ffe10b672b7a222569c4
wenjiechen <wendychen9395@gmail.com>
2021-01-03 13:57:55 -0500

    fix bugs

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -22,14 +22,15 @@ const Skills = () => {
         <Grid container >
           <Grid item md={6} sm={6} xs={12}>
             <Skill title='JavaScript' progress={ 90 } />
-            <Skill title='React' progress={ 85 } />                           
+            <Skill title='React' progress={ 90 } />                           
             <Skill title='Python' progress={ 80 } />
-            <Skill title='Git' progress={ 70 } />                                 
+            <Skill title='Git' progress={ 80 } />                                 
           </Grid>
           <Grid item md={6} sm={6} xs={12}>
             <Skill title='TypeScript' progress={ 80 } />
-            <Skill title='HTML / CSS' progress={ 85 } />
-            <Skill title='SQL' progress={ 80 } /> 
+            <Skill title='HTML / CSS' progress={ 90 } />
+            <Skill title='NextJS' progress={ 80 } /> 
+            <Skill title='SQL' progress={ 85 } /> 
           </Grid>
         </Grid> 
       </div>
